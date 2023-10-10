7b4384dd463523d3d681d51a8b5f260a9cbffa66
maham qadeer <maham.qadeer2003@gmail.com>
2023-10-10 22:20:13 +0500

    node class for doubly list

--- q2.java
+++ q2.java
@@ -1,3 +1,20 @@
+class node{
+    public char data;
+    public node next;
+    public node prev;
+
+    public node(){
+        this.data = ' ';
+        this.next=null;
+        this.prev=null;
+    }
+    public node(char data){
+        this.data = data;
+        this.next=null;
+        this.prev=null;
+    }
+}
+
 public class q2{
     public static void main(String[] args){
         System.out.println("First non repeating char java");
