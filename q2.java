1a56a5d14facd01a03ac73d398037c4b2ad68707
maham qadeer <maham.qadeer2003@gmail.com>
2023-10-10 22:58:34 +0500

    non repeating class created

--- q2.java
+++ q2.java
@@ -1,3 +1,4 @@
+import java.util.Hashtable;
 class node{
     public char data;
     public node next;
@@ -14,9 +15,62 @@ class node{
         this.prev=null;
     }
 }
+class FirstNonRepeatingStream{
+    public node head;
+    public node tail;
+    int size;
 
+    public FirstNonRepeatingStream(){
+        this.head=null;
+        this.tail=null;
+        size=0;
+    }
+    public void add(char data){
+        node temp= new node(data);
+        if(head==null){
+            head=temp;
+            tail=temp;
+        }
+        else{
+            tail.next=temp;
+            temp.prev=tail;
+            tail=temp;
+        }
+        size++;
+    }
+    public char getFirstNonRepeating(){
+        int s=0;
+        node temp=head;
+       if(temp==null){
+            return '-';
+       }
+       else if(temp.data!=temp.next.data && s!=size){
+            ++s;
+            if(temp.data!=temp.next.next.data && s!=size ){
+                ++s;
+                if(temp.data!=temp.next.next.next.data && s!=size ){
+                ++s;
+                return temp.data;
+                }
+                return temp.data;
+            }
+            return temp.data;
+       }
+       else{
+            temp=temp.next;
+       }
+       return '-';
+
+    }
+}
 public class q2{
     public static void main(String[] args){
         System.out.println("First non repeating char java");
+        FirstNonRepeatingStream fnrs=new FirstNonRepeatingStream();
+        fnrs.add('a');
+        fnrs.add('b');
+        fnrs.add('a');
+        System.out.println(fnrs.getFirstNonRepeating());
+
     }
 }
