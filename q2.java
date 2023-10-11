439261baff2b7155d8c1b3e754430e054768a5bc
maham qadeer <maham.qadeer2003@gmail.com>
2023-10-11 21:01:11 +0500

    create node and list class

--- q2.java
+++ q2.java
@@ -1,76 +1,87 @@
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.LinkedList;
 class node{
     public char data;
+    public int count;
     public node next;
-    public node prev;
 
     public node(){
         this.data = ' ';
+        this.count=0;
         this.next=null;
-        this.prev=null;
     }
     public node(char data){
         this.data = data;
+        this.count=0;
         this.next=null;
-        this.prev=null;
+    }
+    public char getData(){
+        return data;
+    }
+    public int getCount(){
+        return count;
+    }
+    public void setCount(int count){
+        this.count=count;
     }
 }
-class FirstNonRepeatingStream{
-    public node head;
-    public node tail;
-    int size;
-
-    public FirstNonRepeatingStream(){
-        this.head=null;
-        this.tail=null;
-        size=0;
+class linkedList{
+    private node head;
+    ArrayList tempList;
+    public linkedList(){
+        this.head=new node();
+        tempList=new ArrayList<Character>();
     }
-    public void add(char data){
-        node temp= new node(data);
+    public node getNode(node n){
+        return n;
+    }
+    public void add(char c){
+        node temp=new node(c);
         if(head==null){
             head=temp;
-            tail=temp;
+            
         }
         else{
-            tail.next=temp;
-            temp.prev=tail;
-            tail=temp;
+            temp.next=head;
+            head=temp;
         }
-        size++;
+        tempList.add(temp);
     }
-    public char getFirstNonRepeating(){
-        int s=0;
-        node temp=head;
-       if(temp==null){
-            return '-';
-       }
-       else if(temp.data!=temp.next.data && s!=size){
-            ++s;
-            if(temp.data!=temp.next.next.data && s!=size ){
-                ++s;
-                if(temp.data!=temp.next.next.next.data && s!=size ){
-                ++s;
-                return temp.data;
+    public boolean contains(char c){
+        if(tempList.contains(c)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void sort(){
+        node curr=head;
+        while (curr != null) {
+            node min= curr;
+            node temp = curr.next;
+            while (temp != null) {
+                if (temp.count < min.count) {
+                    min = temp;
                 }
-                return temp.data;
+                temp = temp.next;
             }
-            return temp.data;
-       }
-       else{
-            temp=temp.next;
-       }
-       return '-';
-
+            char tempData = curr.data;
+            int tempCount = curr.count;
+            curr.data = min.data;
+            curr.count = min.count;
+            min.data = tempData;
+            min.count = tempCount;
+            curr = curr.next;
     }
 }
+
+
 public class q2{
     public static void main(String[] args){
         System.out.println("First non repeating char java");
-        FirstNonRepeatingStream fnrs=new FirstNonRepeatingStream();
-        fnrs.add('a');
-        fnrs.add('b');
-        fnrs.add('a');
-        System.out.println(fnrs.getFirstNonRepeating());
+        
+       
 
     }
 }
